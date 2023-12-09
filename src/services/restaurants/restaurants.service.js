f8b00fa1fe2af3d626f1e141dd9bcfdc1622d3d8
Aaoz Gull <aaozgull@gmail.com>
2023-12-09 08:32:12 +0500

    hook up context with data & add activityindicator

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,10 +1,7 @@
-import { mocks } from "./mock";
+import { mocks, mockImages } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = ({
-  location = "37.7749295,-122.4194155",
-}) => {
-  console.log(mocks);
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
@@ -14,22 +11,17 @@ export const restaurantsRequest = ({
   });
 };
 
-const restaurantsTransform = ({ results = [] }) => {
-  const mappedResult = results.map((restaurant) => {
+export const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    restaurant.photos = restaurant.photos.map((p) => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
+
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.busines_status === "CLOSED_TEMPORARILY",
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
     };
   });
-  return camelize(mappedResult);
+  return camelize(mappedResults);
 };
-
-restaurantsRequest()
-  .then(restaurantsTransform)
-  .then((transformResponse) => {
-    console.log(transformResponse);
-  })
-  .catch((err) => {
-    console.log("Error");
-  });
