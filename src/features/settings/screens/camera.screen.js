0a460c9e3f041abd1f847e7f0b42fd368d17d155
Aaoz Gull <aaozgull@gmail.com>
2023-12-31 14:17:49 +0500

    Add profile pic feature & sae it in the mobile

---
+++ src/features/settings/screens/camera.screen.js
@@ -0,0 +1,70 @@
+import React, { useRef, useState, useEffect, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Camera, CameraType } from "expo-camera";
+import { Button } from "react-native-paper";
+
+import { CustomText } from "../../../components/typography/text.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import styled from "../../../infrastructure/theme";
+
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraContainer = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  top: 525px;
+  left: 140px;
+`;
+
+const CameraScreen = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(false);
+  const { user } = useContext(AuthenticationContext);
+  const cameraRef = useRef(); //<Camera | null>();
+
+  const snap = async () => {
+    if (cameraRef && cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      //console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { granted } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(granted);
+    })();
+  }, []);
+
+  if (hasPermission === false) {
+    return <CustomText>No access to camera</CustomText>;
+  }
+
+  return (
+    <CameraContainer>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        ratio={"16:9"}
+        type={CameraType.front}
+        onCameraReady={() => {
+          console.log("Camera Ready");
+        }}
+      ></ProfileCamera>
+
+      <CameraButton onPress={snap}>Snap!</CameraButton>
+    </CameraContainer>
+  );
+};
+
+export default CameraScreen;
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components/native";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { List, Avatar } from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -17,10 +18,29 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+  useFocusEffect(
+    useCallback(() => {
+      getProfilePicture(user);
+    }, [user])
+  );
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        {!photo && (
+          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        )}
+        {photo && (
+          <Avatar.Image
+            size={180}
+            source={{ uri: photo }}
+            backgroundColor="#2182BD"
+          />
+        )}
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
